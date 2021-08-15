1fc49198c615211de8b3c9ad25f14bb5f2fa67aa
chance221 <kelly292@mail.nmc.edu>
2021-08-15 17:44:03 -0400

    first commit

---
+++ Calculator/Models/Calculate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace Calculator.Models
+{
+    public class Calculate : INotifyPropertyChanged 
+    {
+        private double firstNumber = 0;
+        private double secondNumber = 0;
+        private string operation = "";
+        private string errorMessage = ""; 
+
+        public double FirstNumber
+        {
+            get
+            {
+                return firstNumber;
+            }
+            set
+            {
+                firstNumber = value;
+                OnPropertyChanged("FirstNumber");
+            }
+        }
+
+        public double SecondNumber
+        {
+            get
+            {
+                return secondNumber;
+            }
+            set
+            {
+                secondNumber = value;
+                OnPropertyChanged("SecondNumber");
+            }
+        }
+
+        public string Operation
+        {
+            get
+            {
+                return operation;
+            }
+            set
+            {
+                operation = value;
+                OnPropertyChanged("Operation");
+            }
+        }
+
+
+        public string ErrorMessage
+        {
+            get
+            {
+                return errorMessage;
+            }
+            set
+            {
+                errorMessage = value;
+                OnPropertyChanged("ErrorMessage");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Calculator/ViewModels/CalculateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.ViewModels
+{
+    public class CalculateViewModel
+    {
+        private Calculate _calculator;
+
+        public CalculateViewModel()
+        {
+
+        }
+
+    }
+}
